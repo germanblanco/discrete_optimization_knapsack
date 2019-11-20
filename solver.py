a5febf69aa88a047fbfc6a62ea2190ddba62bf0d
German Blanco <german.blanco@gmail.com>
2019-11-20 18:35:26 +0000

    only override value if better than greedy

--- solver.py
+++ solver.py
@@ -33,7 +33,7 @@ def find_gcd(list):
 def _normalize_capacity(items, capacity):
     item_weights = [item.weight for item in items]
     factor = find_gcd(item_weights)
-    new_items = [Item(item.index, item.value, int(item.weight/factor)) for item in items]
+    new_items = [Item(item.index, item.value, int(item.weight/factor), item.density) for item in items]
     new_capacity = int(capacity/factor)
     return new_items, new_capacity, factor
 
@@ -55,7 +55,7 @@ def _dynamic_programming(items, capacity, taken):
                     else:
                         the_matrix[row][column] = value_with
     row = capacity
-    for column in reversed(range(len(items)+1)):
+    for column in reversed(range(1, len(items)+1)):
         if the_matrix[row][column] == the_matrix[row][column-1]:
             taken[column-1] = 0
         else:
@@ -63,27 +63,67 @@ def _dynamic_programming(items, capacity, taken):
             row = row - items[column-1].weight
     return taken
 
-def _branch_and_bound(items, capacity, taken, index=0, value=0):
+def _branch_and_bound(items, capacity, taken, index=0, value=0, node_limit=10000000):
     item = items[index]
+    if node_limit != 0:
+         _branch_and_bound.current_node_counter = node_limit
+         _branch_and_bound.current_solution = None
+         _branch_and_bound.current_value = None
+    else:
+        current_counter = _branch_and_bound.current_node_counter - 1
+        current_solution = _branch_and_bound.current_solution
+        current_value = _branch_and_bound.current_value
+        if current_counter is None or current_counter == 0:
+            return current_solution, current_value, 0
+        else:
+            _branch_and_bound.current_node_counter = current_counter
+
+    result_taken = None
+    result_value = None
+    result_optimal = None
     if index == len(items)-1:
         if capacity >= item.weight:
             taken_copy = taken.copy()
             taken_copy[index] = 1
-            return value + item.value, taken_copy
+            result_taken = taken_copy
+            result_value = value + item.value
+            result_optimal = 1
         else:
-            return value, taken
+            result_taken = taken
+            result_value = value
+            result_optimal = 1
     else:
-        value_without, taken_without = _branch_and_bound(items, capacity, taken, index+1, value)
-        if capacity >= item.weight:
+        taken_without, value_without, optimal = _branch_and_bound(items,
+                                                                  capacity,
+                                                                  taken,
+                                                                  index+1,
+                                                                  value,
+                                                                  0)
+        if capacity >= item.weight and optimal == 1:
             taken_copy = taken.copy()
             taken_copy[index] = 1
-            value_with, taken_with = _branch_and_bound(items, capacity-item.weight, taken_copy, index+1, value+item.value)
+            taken_with, value_with, optimal = _branch_and_bound(items,
+                                                                capacity-item.weight,
+                                                                taken_copy,
+                                                                index+1,
+                                                                value+item.value,
+                                                                0)
             if value_with > value_without:
-                return value_with, taken_with
+                result_taken = taken_with
+                result_value = value_with
             else:
-                return value_without, taken_without
+                result_taken = taken_without
+                result_value = value_without
         else:
-            return value_without, taken_without
+            result_taken = taken_without
+            result_value = value_without
+        result_optimal = optimal
+    current_solution = _branch_and_bound.current_solution
+    current_value = _branch_and_bound.current_value
+    if current_value is None or result_value > current_value:
+        _branch_and_bound.current_solution = result_taken
+        _branch_and_bound.current_value = result_value
+    return result_taken, result_value, result_optimal
 
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
@@ -107,12 +147,22 @@ def solve_it(input_data):
     taken = [0]*len(items)
     optimal = 0
 
-    if (len(items) * capacity) < 1000000000:
-        taken = _dynamic_programming(items, capacity, taken)
+    new_items, new_capacity, _ = _normalize_capacity(items, capacity)
+    if (len(items) * new_capacity) < 1000000000:
+        taken = _dynamic_programming(new_items, new_capacity, taken)
+        value = sum([item.value for item in items if taken[item.index] == 1])
         optimal = 1
 
     if optimal == 0:
         taken = _density_greedy(items, capacity, taken)
+        value = sum([item.value for item in items if taken[item.index] == 1])
+
+    if optimal == 0:
+        taken_copy = taken.copy()
+        taken_copy, new_value, optimal = _branch_and_bound(items, capacity, taken_copy)
+        if (new_value > value):
+            taken = taken_copy
+            value = new_value
 
     value = sum([item.value for item in items if taken[item.index] == 1])
     
